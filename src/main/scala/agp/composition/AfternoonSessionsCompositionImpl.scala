243e1711187627584a0d1c2f0dd935a805d7db7e
Andrei Gaponenko <andrey.gaponenko@simbirsoft.com>
2019-07-05 11:12:46 +0300

    Removed unnecessary code

--- src/main/scala/agp/composition/AfternoonSessionsCompositionImpl.scala
+++ src/main/scala/agp/composition/AfternoonSessionsCompositionImpl.scala
@@ -1,8 +1,7 @@
 package agp.composition
 
+import agp.Utils.RichSeq
 import agp.vo.{AfternoonSession, Talk}
-
-import scala.annotation.tailrec
 
 private[composition] class AfternoonSessionsCompositionImpl(val requiredSessionsNumber: Int)
   extends (Set[Talk] => AfternoonSessionsCompositionResult) {
@@ -13,7 +12,7 @@ private[composition] class AfternoonSessionsCompositionImpl(val requiredSessions
 
   override def apply(talks: Set[Talk]): Result = {
     validateNumberOf(talks)
-    new Result(sessions = composeSessionsFrom(talks), unusedTalks = Set.empty)
+    new Result(sessions = newSessionsFrom(talks), unusedTalks = Set())
   }
 
   private def validateNumberOf(talks: Set[Talk]): Unit = {
@@ -21,27 +20,8 @@ private[composition] class AfternoonSessionsCompositionImpl(val requiredSessions
       s"Talks.size must be >= $requiredSessionsNumber.")
   }
 
-  private def composeSessionsFrom(talks: Set[Talk]): Set[AfternoonSession] = {
-    val idealSessionSize = talks.size / requiredSessionsNumber
-    type T = Talk
-
-    @tailrec
-    def divideEqually(undivided: Seq[T], divided: List[Seq[T]], extra: Int): List[Seq[T]] =
-      if (undivided.isEmpty) divided else {
-        // take X elements that ideally should be in session,
-        // but if there is extra, take +1 and decrement extra
-        val (splitIndex, newExtra) = if (extra == 0) (idealSessionSize, 0)
-                                     else (idealSessionSize + 1, extra - 1)
-
-        val (newDivided, remaining) = undivided.splitAt(splitIndex)
-        divideEqually(remaining, newDivided :: divided, newExtra)
-      }
-
-    val dividedTalks: Set[Seq[T]] = divideEqually(
-      undivided = talks.toList,
-      divided = Nil,
-      extra = talks.size % requiredSessionsNumber).toSet
-
-    dividedTalks map (list => AfternoonSession(list.toSet))
-  }
+  private def newSessionsFrom(talks: Set[Talk]): Set[AfternoonSession] =
+    talks.toList.equallyDividedInto(requiredSessionsNumber)
+      .map(talksList => AfternoonSession(talksList.toSet))
+      .toSet
 }
