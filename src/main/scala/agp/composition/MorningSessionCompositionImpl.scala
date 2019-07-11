518738b9cb9a514c2d468d1df8f2000456a4f93f
Andrei Gaponenko <andrey.gaponenko@simbirsoft.com>
2019-07-11 10:01:23 +0300

    A bit of refactoring

--- src/main/scala/agp/composition/MorningSessionCompositionImpl.scala
+++ src/main/scala/agp/composition/MorningSessionCompositionImpl.scala
@@ -14,20 +14,20 @@ final class MorningSessionCompositionImpl(
   override def apply(talks: Set[Talk]): Result = {
     require(talks.nonEmpty, "At least one talk is required.")
 
-    findSuitableCombinationsAmong(talks).collectFirst {
-      case suitableCombination => new Result(
-        session = MorningSession(suitableCombination),
-        unusedTalks = talks except suitableCombination
-      )
-    }.getOrElse(throw newException)
+    findSuitableCombinationsAmong(talks)
+      .collectFirst {
+        case suitableTalks => new Result(
+          session = MorningSession(suitableTalks),
+          unusedTalks = talks except suitableTalks)
+
+      } getOrElse (throw newException)
   }
 
-  /* method alias for better comprehension of what knapsack solution is supposed to do */
-  private def findSuitableCombinationsAmong(talks: Set[Talk]): TalksCombinations = knapsackSolution(talks)
+  /* alias for better comprehension of what knapsack solution is supposed to do */
+  def findSuitableCombinationsAmong(talks: Set[Talk]) = knapsackSolution(talks)
 
   private def newException = composition.Exception(
     "Failed to compose MorningSession with knapsack solution " +
     "for provided talks, because no possible combination " +
-    "of talks conforms to required goal duration of event."
-  )
+    "of talks conforms to required goal duration of event.")
 }
