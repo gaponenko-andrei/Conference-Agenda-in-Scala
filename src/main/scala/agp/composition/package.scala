3a4c8e5c64f9b6ced60574699919eb4f858b289c
Andrei Gaponenko <andrey.gaponenko@simbirsoft.com>
2019-07-22 08:51:11 +0300

    Scaladocs for composition package object

--- src/main/scala/agp/composition/package.scala
+++ src/main/scala/agp/composition/package.scala
@@ -6,31 +6,57 @@ import org.scalactic.Or
 
 package object composition {
 
-  /** Alias for package use only, bound to package-specific exception */
-  private[composition] type ExceptionOr[T] = T Or composition.Exception
-
-  /* todo scaladoc */
+  /** Definition of knapsack solution for talks that returns all
+    * combinations of given talks according to it's implementation
+    * or `IllegalArgumentException` if requirements were not met
+    */
   type KnapsackSolution = Set[Talk] => OnMetReq[TalksCombinations]
 
-  /* todo scaladoc */
-  type MorningSessionsCompositionResult = SessionsCompositionResult[MorningSession]
+  /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+  /*                          Sessions Compositions                          */
+  /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
 
-  /* todo scaladoc */
-  type AfternoonSessionsCompositionResult = SessionsCompositionResult[AfternoonSession]
+  /** Definition of function producing [[agp.vo.Session]] from talks */
+  type SessionsComposition[S <: Session] = Set[Talk] => ExceptionOr[S]
 
-  /* todo scaladoc */
-  type MorningSessionsComposition = Set[Talk] => ExceptionOr[MorningSessionsCompositionResult]
+  /** Definition of function producing [[agp.vo.MorningSession]] from talks */
+  type MorningSessionsComposition = SessionsComposition[MorningSession]
 
-  /* todo scaladoc */
-  type AfternoonSessionsComposition = Set[Talk] => ExceptionOr[AfternoonSessionsCompositionResult]
+  /** Definition of function producing [[agp.vo.AfternoonSession]] from talks */
+  type AfternoonSessionsComposition = SessionsComposition[AfternoonSession]
 
-  /** Auxiliary type to return composed sessions
-    * along with talks unused during composition
+  /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+  /*                       Sessions Composition Result                       */
+  /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+
+  /** Holder to keep composed [[agp.vo.Session]] along with
+    * talks that were provided for it's composition, but were
+    * left unused by impl of [[agp.composition.SessionsComposition]]
     */
   final case class SessionsCompositionResult[S <: Session](sessions: Set[S], unusedTalks: Set[Talk])
 
-  /** Specialized exception that should be thrown by
-    * every component of the package in case of failure
+  /** Holder to keep composed [[agp.vo.MorningSession]] along with
+    * talks that were provided for it's composition, but were left
+    * unused by impl of [[agp.composition.MorningSessionsComposition]]
     */
-  final case class Exception(message: String, cause: Throwable = null) extends RuntimeException(message, cause)
+  type MorningSessionsCompositionResult = SessionsCompositionResult[MorningSession]
+
+  /** Holder to keep composed [[agp.vo.AfternoonSession]] along with
+    * talks that were provided for it's composition, but were left
+    * unused by impl of [[agp.composition.AfternoonSessionsComposition]]
+    */
+  type AfternoonSessionsCompositionResult = SessionsCompositionResult[AfternoonSession]
+
+  /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+  /*                             Error Handling                              */
+  /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+
+  /** Alias for package use only, bound to package-specific exception */
+  private[composition] type ExceptionOr[T] = T Or composition.Exception
+
+  /** Specialized exception that should be thrown by
+    * every component of the package in case of error
+    */
+  final case class Exception(message: String, cause: Throwable = null)
+    extends RuntimeException(message, cause)
 }
