087c08c534c73d6dac2ff76085d2703521016655
Andrei Gaponenko <andrey.gaponenko@simbirsoft.com>
2019-07-23 09:02:51 +0300

    + Scaladocs for scheduling PO

--- src/main/scala/agp/scheduling/package.scala
+++ src/main/scala/agp/scheduling/package.scala
@@ -7,10 +7,15 @@ import scala.language.higherKinds
 
 package object scheduling {
 
-  /* specialized exception that should be thrown by every component of the package in case of failure */
-  final case class Exception (message: String, cause: Throwable = null) extends RuntimeException(message, cause)
+  /** Definition of function producing [[agp.scheduling.ConferenceTrack]] from talks */
+  type ConferenceTracksScheduling = Set[Talk] => ExceptionOr[Set[ConferenceTrack]]
 
+  /** Alias for package use only, bound to package-specific exception */
   private[scheduling] type ExceptionOr[T] = T Or scheduling.Exception
 
-  type ConferenceTracksScheduling = Set[Talk] => ExceptionOr[Set[ConferenceTrack]]
+  /** Specialized exception that should be thrown by
+    * every component of the package in case of error
+    */
+  final case class Exception (message: String, cause: Throwable = null)
+    extends RuntimeException(message, cause)
 }
